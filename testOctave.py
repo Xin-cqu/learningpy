0d7b15b83256aad3ad2c2fe356a71aed20d000cd
Arthas-cqu <nclxwen@gmail.com>
2015-11-27 15:10:23 +0800

    examples

--- testOctave.py
+++ testOctave.py
@@ -1,128 +1,76 @@
 __author__ = 'xinwen'
-import numpy
-import os
-import scipy.io as sio
 import tensorflow as tf
+import numpy
 import matplotlib.pyplot as plt
 
+rng = numpy.random
 
-class DataSet(object):
-    def __init__(self, images, labels, fake_data=False):
-        if fake_data:
-            self._num_examples = 10000
-        else:
-            assert images.shape[0] == labels.shape[0], (
-                "images.shape: %s labels.shape: %s" % (images.shape,
-                                                       labels.shape))
-            self._num_examples = 5000
-            # Convert shape from [num examples, rows, columns, depth]
-            # to [num examples, rows*columns] (assuming depth == 1)
-            assert images.shape[3] == 1
-            images = images.reshape(images.shape[0],
-                                    images.shape[1] * images.shape[2])
-            # Convert from [0, 255] -> [0.0, 1.0].
-            images = images.astype(numpy.float32)
-            images = numpy.multiply(images, 1.0 / 255.0)
-        self._images = images
-        self._labels = labels
-        self._epochs_completed = 0
-        self._index_in_epoch = 0
+# Parameters
+learning_rate = 0.01
+training_epochs = 2000
+display_step = 100
 
-    @property
-    def images(self):
-        return self._images
+# Training Data
+train_X = numpy.asarray(
+    [3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167, 7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1])
+train_Y = numpy.asarray(
+    [1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221, 2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3])
+n_samples = train_X.shape[0]
 
-    @property
-    def labels(self):
-        return self._labels
+# tf Graph Input
+X = tf.placeholder("float")
+Y = tf.placeholder("float")
 
-    @property
-    def num_examples(self):
-        return self._num_examples
+# Create Model
 
-    @property
-    def epochs_completed(self):
-        return self._epochs_completed
+# Set model weights
+W = tf.Variable(rng.randn(), name="weight")
+b = tf.Variable(rng.randn(), name="bias")
 
-    def next_batch(self, batch_size, fake_data=False):
-        """Return the next `batch_size` examples from this data set."""
-        if fake_data:
-            fake_image = [1.0 for _ in xrange(400)]
-            fake_label = 0
-            return [fake_image for _ in xrange(batch_size)], [
-                fake_label for _ in xrange(batch_size)]
-        start = self._index_in_epoch
-        self._index_in_epoch += batch_size
-        if self._index_in_epoch > self._num_examples:
-            # Finished epoch
-            self._epochs_completed += 1
-            # Shuffle the data
-            perm = numpy.arange(self._num_examples)
-            numpy.random.shuffle(perm)
-            self._images = self._images[perm]
-            self._labels = self._labels[perm]
-            # Start next epoch
-            start = 0
-            self._index_in_epoch = batch_size
-            assert batch_size <= self._num_examples
-        end = self._index_in_epoch
-        return self._images[start:end], self._labels[start:end]
+# Construct a linear model
+activation = tf.add(tf.mul(X, W), b)
 
+# Minimize the squared errors
+cost = tf.reduce_sum(tf.pow(activation - Y, 2)) / (2 * n_samples)  # L2 loss
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)  # Gradient descent
 
-def read_data_sets(train_dir):
-    class DataSet(object):
-        pass
-
-    data_sets = DataSet()
-    VALIDATION_SIZE = 2000
-    TEST_SIZE = 1000
-    data = sio.loadmat(train_dir)
-    train_images = data['X']
-    train_labels = data['y']
-
-    '''test_images = train_images[:TEST_SIZE]
-    test_labels = train_labels[:TEST_SIZE]
-    train_images = train_images[VALIDATION_SIZE:]
-    train_labels = train_labels[VALIDATION_SIZE:]'''
-    validation_images = train_images[:VALIDATION_SIZE]
-    validation_labels = train_labels[:VALIDATION_SIZE]
-    train_images = train_images[VALIDATION_SIZE:]
-    train_labels = train_labels[VALIDATION_SIZE:]
-    data_sets.train = DataSet(train_images, train_labels)
-    data_sets.validation = DataSet(validation_images, validation_labels)
-    data_sets.test = DataSet(validation_images, validation_labels)
-    return data_sets
-
-
-mnist = read_data_sets('MNIST_data/ex3data1.mat')
-
-VALIDATION_SIZE = 2000
-TEST_SIZE = 1000
-data = sio.loadmat('MNIST_data/ex3data1.mat')
-train_images = data['X']
-train_labels = data['y']
-
-test_images = train_images[:TEST_SIZE]
-test_labels = train_labels[:TEST_SIZE]
-train_images = train_images[VALIDATION_SIZE:]
-train_labels = train_labels[VALIDATION_SIZE:]
-validation_images = train_images[:VALIDATION_SIZE]
-validation_labels = train_labels[:VALIDATION_SIZE]
-train_images = train_images[VALIDATION_SIZE:]
-train_labels = train_labels[VALIDATION_SIZE:]
-x = tf.placeholder("float", [None, 400])
-W = tf.Variable(tf.zeros([400, 10]))
-b = tf.Variable(tf.zeros([10]))
-y = tf.nn.softmax(tf.matmul(x, W) + b)
-y_ = tf.placeholder("float", [None, 10])
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+# Initializing the variables
 init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
-for i in range(1000):
-    batch_xs, batch_ys = mnist.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+
+    # Fit all training data
+    for epoch in range(training_epochs):
+        for (x, y) in zip(train_X, train_Y):
+            sess.run(optimizer, feed_dict={X: x, Y: y})
+
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print "Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(
+                sess.run(cost, feed_dict={X: train_X, Y: train_Y})), \
+                "W=", sess.run(W), "b=", sess.run(b)
+
+    print "Optimization Finished!"
+    training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+    print "Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n'
+
+
+    # Testing example, as requested (Issue #2)
+    test_X = numpy.asarray([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1])
+    test_Y = numpy.asarray([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03])
+
+    print "Testing... (L2 loss Comparison)"
+    testing_cost = sess.run(tf.reduce_sum(tf.pow(activation - Y, 2)) / (2 * test_X.shape[0]),
+                            feed_dict={X: test_X, Y: test_Y})  # same function as cost above
+    print "Testing cost=", testing_cost
+    print "Absolute l2 loss difference:", abs(training_cost - testing_cost)
+
+
+    # Graphic display
+    plt.plot(train_X, train_Y, 'ro', label='Original data')
+    plt.plot(test_X, test_Y, 'bo', label='Testing data')
+    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
+    plt.legend()
+    plt.show()
